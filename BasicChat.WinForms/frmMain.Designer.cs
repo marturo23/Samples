99072c50d9921970463620c4516f96fc11916b35
Henrik Andersson <alfhenrik@gmail.com>
2012-11-23 17:28:09 +1000

    Added WinForms sample

---
+++ BasicChat.WinForms/frmMain.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace BasicChat.WinForms
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMessages = new System.Windows.Forms.ListBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbMessages
+            // 
+            this.lbMessages.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbMessages.FormattingEnabled = true;
+            this.lbMessages.Location = new System.Drawing.Point(0, 0);
+            this.lbMessages.Name = "lbMessages";
+            this.lbMessages.Size = new System.Drawing.Size(463, 290);
+            this.lbMessages.TabIndex = 0;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 297);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(358, 20);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(376, 296);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 326);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.lbMessages);
+            this.Name = "Form1";
+            this.Text = "Basic WinForm Chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbMessages;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ BasicChat.WinForms/frmMain.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.AspNet.SignalR.Client.Hubs;
+using System.Threading.Tasks;
+
+namespace BasicChat.WinForms
+{
+    public partial class frmMain : Form
+    {
+        HubConnection _connection;
+        IHubProxy _chat;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            _connection = new HubConnection("http://localhost:44914");
+            _chat = _connection.CreateHubProxy("Chat");
+
+            _chat.On<string>("send", MessageReceived);
+
+            _connection.Start();
+        }
+
+        private void MessageReceived(string message)
+        {
+            this.Invoke(new MethodInvoker(() =>
+                lbMessages.Items.Add(message)
+            ));
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtMessage.Text))
+            {
+                _chat.Invoke("send", "WinForm Client: " + txtMessage.Text);
+            }
+            txtMessage.Text = String.Empty;
+        }
+    }
+}
